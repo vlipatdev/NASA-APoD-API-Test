7e13c6c9a9616f439e6cc4909b99f5330a660aec
vlipat <vlipatdev@users.noreply.github.com>
2019-02-03 22:41:46 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$.ajax({
+	url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY, //Apply for an API Key: https://api.nasa.gov/index.html#apply-for-an-api-key
+	success: function(data) {
+		if(data.media_type === 'image') {
+			$('#video-container').css('display', 'none');
+			$('#image-container').attr('href', data.url); 
+			$('#image').attr('src', data.url);
+			$('#image').attr('alt', data.title);
+		} else {
+			$('#image-container').css('display', 'none'); 
+			$('#video').attr('src', data.url);
+		}
+
+		if('copyright' in data) {
+			$('#owner').text(data.copyright);
+		} else {
+			$('#owner').text('Public Domain');
+		}
+
+		$('#title').text(data.title);
+		$('#date').text(data.date);
+		$('#explanation').text(data.explanation);
+	},
+
+	error: function() {
+		$('#title').text('An error has occurred.');
+		$('#copyright').text('');
+
+	}
+})
