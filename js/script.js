4c3066a5890715cace92e7be00120eac3eacb398
vlipat <vlipatdev@users.noreply.github.com>
2019-02-05 21:27:09 +0800

    Add night mode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+$('#darkmode-icon').click(function() {
+	$('body').toggleClass('body-darkmode');
+	$('svg').toggleClass('svg-darkmode');
+})
+
 $.ajax({
 	url: 'https://api.nasa.gov/planetary/apod?api_key=H0SZyM87XABTNLMJyMApX41QSQn4tbBjI5bs1Pg7', //Apply for an API Key: https://api.nasa.gov/index.html#apply-for-an-api-key
 	success: function(data) {
@@ -7,13 +12,13 @@ $.ajax({
 			$('#image').attr('src', data.url);
 
 			if('hdurl' in data) {
-				$('#image-container').attr('href', data.hdurl); 
+				$('#image-container').attr('href', data.hdurl);
 			} else {
-				$('#image-container').attr('href', data.url); 
+				$('#image-container').attr('href', data.url);
 			}
 
 		} else {
-			$('#image-container').css('display', 'none'); 
+			$('#image-container').css('display', 'none');
 			$('#video').attr('src', data.url);
 		}
 
