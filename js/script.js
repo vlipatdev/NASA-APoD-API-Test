134d049060fc7dad717dada622ba75d61d2577f2
vlipatdev <vlipat.dev@gmail.com>
2019-05-10 21:47:53 +0800

    Replace jQuery with axios

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-$('#darkmode-icon').click(function() {
-	$('body').toggleClass('body-darkmode');
-	$('svg').toggleClass('svg-darkmode');
-})
+document.querySelector('#darkmode-icon').addEventListener('click', () => {
+	document.body.classList.toggle('body-darkmode');
+	Array.from(document.querySelectorAll('svg')).map(el => el.classList.toggle('svg-darkmode')); //not working
+});
 
-$.ajax({
-	url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', //Apply for an API Key: https://api.nasa.gov/index.html#apply-for-an-api-key
-	success: function(data) {
-		if(data.media_type === 'image') {
-			$('#video-container').css('display', 'none');
-			$('#image-container').attr('href', data.hdurl);
-			$('#image').attr('alt', data.title);
-			$('#image').attr('src', data.url);
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+	.then(result => {
+		const data = result.data
+		console.log(data.data)
 
+		if(data.media_type ==='image') {
+			document.querySelector('#video-container').style.display = 'none';
+			document.querySelector('#image-container').setAttribute('href', data.hdurl);
+			document.querySelector('#image').setAttribute('alt', data.title);
+			document.querySelector('#image').setAttribute('src', data.url);
 		} else {
-			$('#image-container').css('display', 'none');
-			$('#video').attr('src', data.url);
-		}
+			document.querySelector('#image-container').style.display = 'none';
+			document.querySelector('#video-container').setAttribute('src', data.hdurl);
+		};
 
 		if('copyright' in data) {
-			$('#copyright').text('Image Credit and Copyright: ' + data.copyright);
+			document.querySelector('#copyright').textContent = `Image Credit and Copyright: ${data.copyright}`;
 		} else {
-			$('#copyright').text('Public Domain');
-		}
+			document.querySelector('#copyright').textContent = 'Public Domain';
+		};
 
-		var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-		var todayDate = new Date(data.date);
-		function dateFormat(d) {
-			return monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear();
-		}
+		const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		const todayDate = new Date(data.date);
+		console.log(todayDate);
+		const dateFormat = d => `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
 
-		$('#title').text(data.title);
-		$('#loading').css('display', 'none');
-		$('#date').text(dateFormat(todayDate));
-		$('#explanation').text(data.explanation);
-	},
+		document.querySelector('#title').textContent = data.title;
+		document.querySelector('#loading').style.display = 'none';
+		document.querySelector('#date').textContent = dateFormat(todayDate);
+		document.querySelector('#explanation').textContent = data.explanation;
+		})
 
-	error: function() {
-		$('#APOD').text('An error has occurred. Refresh the page.');
-		$('#loading').css('display', 'none');
-	}
-})
+	.catch(() => {
+		document.querySelector('#APOD').textContent = 'An error has occurred. Refresh the page.';
+		document.querySelector('#loading').style.display = 'none';
+	});
