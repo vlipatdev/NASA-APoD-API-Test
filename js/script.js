2b37f63c6e1c52f2d1578af9e37d129d54d6a405
Val Lipat <vlipat.dev@gmail.com>
2019-06-23 09:13:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
 		const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		const todayDate = new Date(data.date);
-		const dateFormat = d => `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+		const formatDate = d => `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
 
 		document.querySelector('.title').textContent = data.title;
 		document.querySelector('.loading').style.display = 'none';
-		document.querySelector('.date').textContent = dateFormat(todayDate);
+		document.querySelector('.date').textContent = formatDate(todayDate);
 		document.querySelector('.explanation').textContent = data.explanation;
 		})
 
